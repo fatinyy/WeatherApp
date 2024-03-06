d2ace47cf5ccfa2263103a0f42cf2cd43edbe900
fatin <fatiny568@gmail.com>
2024-03-06 15:24:06 +0800

    first commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by aifara on 06/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
